e7658771a2dc422962b5ee56234fcb9d220df322
MattRickS <mattalexshaw@gmail.com>
2020-07-13 11:27:26 +0100

    move writeColor inside main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <camera.hpp>
-#include <color.hpp>
 #include <hittables/hittable_list.hpp>
 #include <hittables/sphere.hpp>
 #include <materials/dielectric.hpp>
@@ -10,6 +9,18 @@
 #include <ray.hpp>
 #include <utils.hpp>
 #include <vec3.hpp>
+
+void writeColor(std::ostream &out, color3 col, int samples)
+{
+    double scale = 1.0 / samples;
+    double r = std::pow(scale * col.r, 0.5);
+    double g = std::pow(scale * col.g, 0.5);
+    double b = std::pow(scale * col.b, 0.5);
+
+    out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
+}
 
 color3 rayColor(const Ray &ray, const Hittable &world, int depth)
 {
